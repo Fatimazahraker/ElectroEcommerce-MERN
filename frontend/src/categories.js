d32c1d9ea9319adb7fdbf3a236840b4e2d19c42a
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2024-04-18 22:25:04 +0100

    update home page and add home.css

---
+++ frontend/src/categories.js
@@ -0,0 +1,16 @@
+const categories = [
+    {
+        id : 1,
+        name: "Technology",
+        img: "https://res.cloudinary.com/dvc52xluq/image/upload/v1711496302/71xTJmbXrjL_iynqdj.jpg",
+    },
+    {
+        id: 2,
+        name: "Phones",
+        img: "https://res.cloudinary.com/dvc52xluq/image/upload/v1711495900/Apple-iPhone-15-Plus-Pink-thumbnail_gyo0wa.png",
+    },
+
+    { id: 3, name: "Laptops", img: "https://res.cloudinary.com/dvc52xluq/image/upload/v1711495971/laptopstopicpage-2048px-2102-3x2-1_jcteps.jpg" },
+];
+
+export default categories;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { Col, Row } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
+import categories from "../categories";
+import "./Home.css";
 
 function Home() {
+
+
   return (
-    <div>Home Page</div>
+    <div>
+      <img src=" https://res.cloudinary.com/dvc52xluq/image/upload/v1711494833/360_F_489280525_nISHfaWCctYBFlyYkTQUkzQwVOPWmyvp_xlql62.jpg" className="home-banner" />
+      <div className="featured-products-container container mt-4">
+        <h2>Last products</h2>
+        {/* last products here */}
+        <div className='d-flex justify-content-center flex-wrap'>
+         
+        </div>
+        <div>
+            <Link to="/category/all" style={{ textAlign: "right", display: "block", textDecoration: "none" }}>
+            See more {">>"}
+             </Link>
+        </div>
+      </div>
+      {/* sale banner */}
+      <div className="sale__banner--container mt-4">
+          <img src="https://res.cloudinary.com/dvc52xluq/image/upload/v1711495522/63456144e2c5a500112d6fca_ci8xcm.jpg" />
+      </div>
+      <div className="recent-products-container container mt-4">
+                <h2>Categories</h2>
+                <Row>
+                    {categories.map((category) => (
+                        <LinkContainer key={category.id} to={`/category/${category.name.toLocaleLowerCase()}`}>
+                            <Col md={4}>
+                                <div style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${category.img})`, gap: "10px" }} className="category-tile">
+                                    {category.name}
+                                </div>
+                            </Col>
+                        </LinkContainer>
+                    ))}
+                </Row>
+            </div>
+    </div>
+
+
   )
 }
 
-export default Home
+export default Home;
