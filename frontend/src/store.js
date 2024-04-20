3668915def97f8fbdcf236b1a1acb2b2593c3979
laila22haz <hazmirlaila316@gmail.com>
2024-04-20 09:14:02 +0100

    Start with redux

---
+++ frontend/src/store.js
@@ -0,0 +1,31 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import productSlice from './features/productSlice';
+import userSlice from './features/userSlice';
+import appApi from './services/appApi';
+
+
+//reducers
+const reducer = combineReducers({
+    user: userSlice,
+    products: productSlice,
+    [appApi.reducerPath]: appApi.reducer,
+});
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    blackList: [appApi.reducerPath, "products"],
+};
+
+// Persist our store
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+// Creating the store
+const store = configureStore({
+    reducer: persistedReducer,
+});
+
+export default store;
